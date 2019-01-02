eef532ba16a8c1e128eb84d3a301510aa2c8db97
Emil VATAI <emil.vatai@gmail.com>
2019-01-02 12:11:49 +0900

    Added new CropGenerator class.

--- generators.py
+++ generators.py
@@ -104,6 +104,45 @@ class BboxEvalGenerator(keras.utils.Sequence):
             img = image.img_to_array(img)
             img = preprocess_input(img)
             inputs_batch[i] = img
+            outputs_batch[i] = keras.utils.to_categorical(wnid_idx, 1000)
+        return inputs_batch, outputs_batch
+
+
+class CropGenerator(keras.utils.Sequence):
+    """
+    Evaluation generator class (Sequence) for ImageNet 2012 with
+    croping.  For now it is ResNet50 specific.
+    """
+    def __init__(self, val_file, img_dir, batch_size):
+        self.img_dir = img_dir
+        self.file_list = []
+        self.category_list = []
+        with open(val_file) as file:
+            for line in file:
+                img_path, cat_str = line.split(" ")
+                cat = int(cat_str)
+                img_path = os.path.join(img_dir, img_path)
+                self.file_list.append(img_path)
+                self.category_list.append(cat)
+        self.batch_size = batch_size
+        self.indices = np.arange(len(self.file_list))
+
+    def __len__(self):
+        return math.ceil(len(self.file_list) / self.batch_size)
+
+    def __getitem__(self, idx):
+        print('Evaluating idx: {}/{}'.format(idx, self.__len__()))
+        inds = self.indices[idx * self.batch_size: (idx + 1) * self.batch_size]
+        inputs_batch = np.zeros([self.batch_size, 224, 224, 3], np.float32)
+        outputs_batch = np.zeros([self.batch_size, 1000], np.float32)
+        for i, j in enumerate(inds):
+            img = image.load_img(self.file_list[j], target_size=(256, 256))
+            # 256 - 224 = 32
+            img = img.crop([16, 16, 224+16, 224+16])
+            img = image.img_to_array(img)
+            img = preprocess_input(img)
+            inputs_batch[i] = img
+            cat = self.category_list[j]
             outputs_batch[i] = keras.utils.to_categorical(cat, 1000)
         return inputs_batch, outputs_batch
 
