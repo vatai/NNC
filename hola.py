48fe9a93a4622d6077915c3137665190ca9a7e9b
Emil VATAI <emil.vatai@gmail.com>
2019-01-04 16:30:23 +0900

    Results added.
    The pickl contain the actual results (gold, is the original, norm and nonorm are the compressed values with and without normalisation. results-combined.txt is the output of the program which was processed by hola. Pickle files can be loaded with var_dic = pickle.load(open(filename,rb)).

---
+++ hola.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+with open(file_path) as file:
+    i = 0
+    gold, norm, nonorm = {}, {}, {}
+    for line in file:
+        trim = line.find('>>>>>>') + 7
+        name_end = trim + line[trim:].find(' ')
+        vec_pos = trim + line[trim:].find('=') + 2
+        name = line[trim:name_end]
+        if i % 4 != 0:
+            vals = eval(line[vec_pos:])
+        if i % 4 == 1:
+            gold[name] = vals
+        if i % 4 == 2:
+            norm[name] = vals
+        if i % 4 == 3:
+            nonorm[name] = vals
+        i += 1
+        
