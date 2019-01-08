26d24bfb7b4c870f4458214e275796e134fe2adf
Emil VATAI <emil.vatai@gmail.com>
2019-01-08 20:16:24 +0900

    Basic aggregation code added.

---
+++ aggregation.py
@@ -0,0 +1,32 @@
+"""This program aggregates the results of compressing the dense layers."""
+
+import pickle
+from pprint import pprint
+from sacred import Experiment
+from sacred.observers import FileStorageObserver
+from utils import get_results_dir
+
+EX = Experiment()
+EX.observers.append(FileStorageObserver.create(get_results_dir(__file__)))
+
+
+@EX.config
+def config():
+    sources = [
+        "./report/weights.pickl",
+        "./report/gold.pickl",
+        "./report/nonorm.pickl",
+        "./report/norm.pickl"
+        ]
+
+
+@EX.automain
+def main(sources):
+    # weights = pickle.load(open(sources[0]))
+    # gold = pickle.load(open(sources[1]))
+    # nonorm = pickle.load(open(sources[2]))
+    weights, gold, nonorm, norm = map(lambda t: pickle.load(open(t, 'rb')), sources)
+    pprint(weights)
+    pprint(gold)
+    pprint(nonorm)
+    pprint(norm)
