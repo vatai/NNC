273dcddb1726ce7b6f49d990db8af0ff9af9ba49
Emil VATAI <emil.vatai@gmail.com>
2019-02-05 07:40:10 +0900

    Some changes to quickie.

--- runscripts/run_quickie.sh
+++ runscripts/run_quickie.sh
@@ -6,8 +6,8 @@ if [ -e /usr/local/anaconda3/lib ]; then
     srun=srun
 fi
 
-for exp in $(seq 11 19); do
-    for norm in True False; do
+for exp in $(seq 19 19); do
+    for norm in True; do
         if [ "$norm" = True ]; then
             name=norm
         else
@@ -18,6 +18,7 @@ for exp in $(seq 11 19); do
         else
             optirun=""
         fi
-	srun bash -c "singularity exec --nv ./singularity/stf-oldv.sif python ./src/compare_with_compression.py with proc_args.norm=False proc_args.epsilon=1e-11 'model_names=[\"resnet50\"]'"
+	# srun bash -c "singularity exec --nv ./singularity/stf-oldv.sif python ./src/compare_with_compression.py with proc_args.norm=False proc_args.epsilon=1e-11 'model_names=[\"resnet50\"]'"
+	python3 ./src/compare_with_compression.py with proc_args.norm=$norm proc_args.epsilon=1e-$exp 'model_names=["resnet50"]' 'gen_args.fast_mode=True'
     done
 done
