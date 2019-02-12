699def2b03cc1479a9beaad8cb0c4861511a80ab
Emil VATAI <emil.vatai@gmail.com>
2019-02-12 12:55:36 +0900

    Updated run_weights.sh.

--- runscripts/run_weights.sh
+++ runscripts/run_weights.sh
@@ -1,21 +1,7 @@
 #!/usr/bin/bash
 #SBATCH
-if [ -e /usr/local/anaconda3/lib ]; then
-    srun=srun
-fi
-
-for exp in $(seq 3 6); do
+for exp in $(seq 11 19); do
     for norm in True False; do
-        if [[ $norm == True ]]; then
-            name=norm
-        else
-            name=nonorm
-        fi
-        if [ -f /usr/bin/optirun ]; then
-            optirun=optirun
-        else
-            optirun=""
-        fi
-        $srun $optirun python3 get_dense_weight_size.py with proc_args.norm=$norm proc_args.epsilon=1e-0$exp
+        python3 src/get_dense_weight_size.py with proc_args.norm=$norm proc_args.epsilon=1e-0$exp
     done
 done
