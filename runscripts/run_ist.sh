436ed2613ccb46722b0b92886417c3a3ecee7751
Emil VATAI <emil.vatai@gmail.com>
2019-02-14 21:51:32 +0900

    Updated ist run script.

--- runscripts/run_ist.sh
+++ runscripts/run_ist.sh
@@ -11,6 +11,14 @@ for exp in 15 25 35; do
         else
             name=nonorm
         fi
-        srun sh -c "singularity exec --nv ./singularity/tf-11-mod python ./src/compare_with_compression.py with proc_args.norm=$norm proc_args.epsilon=0.0$exp json_name=$name-0.0$exp"
+        srun sh -c "singularity exec --nv ./singularity/tf-11-mod \
+		python ./src/compare_with_compression.py with \
+			gen_args.batch_size=64 \
+			eval_args.max_queue_size=14 \
+			eval_args.workers=14 \
+			eval_args.use_multiprocessing=True \
+			proc_args.norm=$norm \
+			proc_args.epsilon=0.0$exp \
+			json_name=$name-0.0$exp"
     done
 done
