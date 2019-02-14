467df59a270aeec33c780729d7bea27227080d14
Emil VATAI <emil.vatai@gmail.com>
2019-02-14 10:20:43 +0900

    Updated run_test_gpu.py

--- runscripts/run_test_gpu.sh
+++ runscripts/run_test_gpu.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 #SBATCH -p p
-#SBATCH -N2
-#SBATCH --ntasks-per-core=1
 #SBATCH --gres=gpu:1
 
-srun python3 -c 'from tensorflow.python.client import device_lib; print(device_lib.list_local_devices())'
+# python3 -c 'from tensorflow.python.client import device_lib; print(device_lib.list_local_devices())'
 
-srun sh -c "singularity exec --nv ./singularity/stf-oldv.sif python3 ./src/nnclib/test_gpu.py"
+srun sh -c "singularity exec --nv ./singularity/tf-11-mod python ./src/nnclib/test_gpu.py"
