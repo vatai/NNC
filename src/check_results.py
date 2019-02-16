af20ee7f15d045061c62ad31c44aa57da6500327
Emil VATAI <emil.vatai@gmail.com>
2019-02-16 12:25:30 +0900

    Added check_results.py.

---
+++ src/check_results.py
@@ -0,0 +1,18 @@
+"""
+Check if the eval* and weight* json files have the same number of
+keys.
+"""
+
+from json import load
+from glob import glob
+from os.path import basename
+
+JSONS = glob("results/combined/*/*/*.json")
+KEYS = sorted(load(open(JSONS[0], 'r')).keys())
+for j in JSONS:
+    json = load(open(j, 'r'))
+    k = sorted(json.keys())
+    if not k == KEYS:
+        print(j, k)
+    else:
+        print("ok: " + basename(j))
