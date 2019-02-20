beeb31f1b2f24760bf9dc1f3c027fb510aafff71
Emil VATAI <emil.vatai@gmail.com>
2019-02-20 23:57:24 +0900

    Updated plot_keras_models.py to read pre-generated weights.

--- src/plot_keras_models.py
+++ src/plot_keras_models.py
@@ -4,54 +4,40 @@ weights.
 """
 
 from glob import glob
+from os.path import splitext, basename, join
 import numpy as np
 import matplotlib.pyplot as plt
-from keras.layers.core import Dense
-from keras.layers.convolutional import Conv2D
-from nnclib.utils import model_dic, reshape_weights
 
 
-def get_same_type_layers(layers, ltype=(Dense, Conv2D)):
-    """Return only Dense or Conv2D layers (or any other type)."""
-    return list(filter(lambda x: isinstance(x, ltype), layers))
-
-
-def proc_dense_layer(name, layer, idx):
+def proc_file(name, base="report/s_shapes"):
     """Process a single layer if it is Dense (or other given type)."""
-    weights = layer.get_weights()
-    dense = weights[0]
-    dense = reshape_weights(dense)
-    sorted_dense = np.sort(dense, axis=1)
-    norms_dense = np.linalg.norm(dense, axis=1)
+    dense = np.load(name)
+    sorted_dense = np.sort(dense, axis=0)
+    norms_dense = np.linalg.norm(dense, axis=0)
 
-    normalised = sorted_dense / norms_dense[:, np.newaxis]
-    fig, ax = plt.subplots(1, 2)
-    ax[0].plot(normalised.T)
-    ax[0].set_title("{}-{} - Normalised".format(name, idx))
-    ax[0].set_ylim(-1, 1)
-    ax[1].plot(sorted_dense.T)
-    ax[1].set_title("{}-{} - Not normalised".format(name, idx))
-    ax[1].set_ylim(-1, 1)
-    # plt.show()
-    fig.savefig("imgs/{}-{}".format(name, idx))
-    plt.close(fig)
+    normalised = sorted_dense / norms_dense[np.newaxis, :]
+    for norm in [0, 1]:
+        title = basename(name)
+        title = splitext(title)[0]
+        title = "{}_nrm{}".format(title, norm)
+        plt.plot(normalised)
+        plt.title(title)
+        plt.ylim(-1, 1)
+        # plt.show()
 
-
-def proc_model(name="vgg16"):
-    """Process one model based on the model name."""
-    model = model_dic[name][0]()
-    layers = get_same_type_layers(model.layers)
-    for idx, layer in enumerate(layers):
-        proc_dense_layer(name, layer, idx)
+        title = join(base, title)
+        print("Saving fig: {}".format(title))
+        plt.savefig(title)
+        plt.close()
 
 
-def proc_all_models():
-    """Process all models."""
-    num_models = len(model_dic.keys())
-    for i, name in enumerate(model_dic.keys()):
-        print("{} - {}/{}".format(name, i+1, num_models))
-        proc_model(name)
+def proc_all_files():
+    """Process all pre-generated weight files."""
+    files = glob("report/weights/*")
+    n = len(files)
+    for i, name in enumerate(files):
+        print("Started {}/{}".format(i, n))
+        proc_file(name)
 
-
-proc_all_models()
+proc_all_files()
 print("Done")
