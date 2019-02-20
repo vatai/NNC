b77eb63c2aace149dcc7eeb8a78712895b714e0f
Emil VATAI <emil.vatai@gmail.com>
2019-02-20 22:43:21 +0900

    Normality test.

---
+++ src/collect_weights.py
@@ -0,0 +1,62 @@
+"""
+This program collects the weights of all the conv2d and dense layers
+to feed it to the jupyter notebook for analysis.
+"""
+
+from os.path import join
+from nnclib.utils import model_dic
+import numpy as np
+from keras.layers.core import Dense
+from keras.layers.convolutional import Conv2D
+
+
+def proc_weights(weights):
+    shape = np.shape(weights)
+    # print('shape1', shape)
+    dim1 = shape[-2]
+    dim2 = shape[-1]
+    for dim in shape[:-2]:
+        dim2 *= dim
+    new_weights = np.reshape(weights, [dim1, dim2])
+    # print('new_shape', np.shape(new_weights))
+    return new_weights
+
+
+def proc_layer(layer):
+    if isinstance(layer, (Dense, Conv2D)):
+        # print('type', type(layer))
+        if isinstance(layer, Dense):
+            typ = 'dense'
+        else:
+            typ = 'conv2d'
+        weights = layer.get_weights()[0]
+        shp = np.shape(weights)
+        shp = 'x'.join(map(str, shp))
+        result = proc_weights(weights)
+        return [typ, shp, result]
+
+def proc_model(model):
+    results = []
+    for idx, layer in enumerate(model.layers):
+        result = proc_layer(layer)
+        if result is not None:
+            results.append([idx] + result)
+    return results
+
+
+def write_results(results, name, base="report/weights"):
+    for idx, typ, shp, result in results:
+        file_name = "{}_{}_{}_{}".format(name, idx, typ, shp)
+        file_name = join(base, file_name)
+        np.save(file_name, result)
+
+
+def proc_all_models():
+    for name, value in model_dic.items():
+        print(name)
+        model = value[0]()
+        results = proc_model(model)
+        write_results(results, name)
+
+
+proc_all_models()
