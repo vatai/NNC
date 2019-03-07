67c14539ca595e503d96ef94f2a8833fa161fc02
Emil VATAI <emil.vatai@gmail.com>
2019-03-07 17:05:40 +0900

    all_shape_plot.py and epsilons.py added.

---
+++ src/all_shape_plot.py
@@ -0,0 +1,51 @@
+"""Make plots for all layers for a given network."""
+import sys
+import multiprocessing
+# sys.path.append("../src")
+
+import numpy as np
+import matplotlib.pyplot as plt
+from nnclib.utils import model_dic
+
+def proc_weights(p):
+    a = np.argsort(p, axis=0)
+    p = np.take_along_axis(p, a, axis=0)
+    norm = np.linalg.norm(p, axis=0)
+    p /= norm
+    fig, ax = plt.subplots()
+    ax.plot(p)
+    return fig
+
+
+def process(model_name):
+    # model_name = model_info[0]
+    print(model_name)
+    # model = model_info[1][0]()
+    model = model_dic[model_name][0]()
+
+    # skip input layer
+    for idx, layer in enumerate(model.layers[1:]):
+        # print(type(layer))
+        weights = layer.get_weights()
+        if isinstance(weights, list) and weights:
+            weights = weights[0]
+        shape = np.shape(weights)
+        # print(shape)
+        if len(shape) > 1:
+            sh1 = shape[-2]
+            sh0 = shape[-1]
+            for d in shape[:-2]:
+                sh0 *= d
+            weights = np.reshape(weights, [sh0, sh1])
+            fig = proc_weights(weights)
+            shape_str = "x".join(map(str, shape))
+            typ = str(type(layer))
+            typ = typ.split('.')[-1][:-2]
+            fig_name = "_".join([model_name, typ, shape_str, str(idx)])
+            fig.savefig(fig_name + ".pdf")
+            fig.savefig(fig_name + ".png")
+            plt.close(fig)
+
+
+pool = multiprocessing.Pool()
+pool.map(process, model_dic.keys())
---
+++ src/epsilons.py
@@ -0,0 +1,75 @@
+"""
+Make table of epsilons from weights.  Besically, for each model
+calculate the max and the average epsilon.
+
+The max and avg epsilon is the max and avg of the layer epsilon.
+
+The layer epsilon is the max of the column epsilon.
+
+The column epsilon is the max - min in the column.
+
+NOTE: These are the actual epsilons from the paper, i.e. "epsilon close" epsilons, not the pruning deltas.
+"""
+
+import glob
+import multiprocessing
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def make_table(d):
+    for model_name, data in d.items():
+        total = 0
+        esum, nsum, emax, nmax = 0, 0, 0, 0
+        for eps, neps in data:
+            emax = max(emax, eps.max())
+            nmax = max(nmax, neps.max())
+            esum += np.sum(eps)
+            nsum += np.sum(neps)
+            total += len(eps)
+        line = [model_name, emax, esum / total, nmax, nsum / total]
+        line = "{:17} & {:7.5} & {:9.5} & {:7.5} & {:9.5} \\\\".format(*line)
+        print(line)
+
+
+def layer_epsilon(layer):
+    norm = np.linalg.norm(layer, axis=0)
+    nlayer = layer / norm
+    nlayer.sort(axis=0)
+    layer.sort(axis=0)
+    # eps = layer.max(axis=1) - layer.min(axis=1)
+    # neps = nlayer.max(axis=1) - nlayer.min(axis=1)
+    eps = np.max(np.abs(layer.T - layer.mean(axis=1)), axis=0)
+    neps = np.max(np.abs(nlayer.T - nlayer.mean(axis=1)), axis=0)
+    # print(layer.shape)
+    # print(norm.shape)
+    # print(eps.shape)
+    # plt.plot(eps)
+    # plt.plot(neps)
+    # plt.plot(layer)
+    # plt.plot(nlayer+0.5)
+    # plt.legend(['e', 'n'])
+    # plt.show()
+    # exit()
+    return eps, neps
+
+
+def proc_all(base="*"):
+    files = sorted(glob.glob(base))
+    d = {}
+    n = len(files)
+    for idx, file in enumerate(files):
+        layer = np.load(file)
+        layer_eps = layer_epsilon(layer)
+        if idx % 100 == 99:
+            print("{}/{} done".format(idx+1, n))
+        model = file.split("_")[0]
+        if model in d:
+            d[model].append(layer_eps)
+        else:
+            d[model] = [layer_eps]
+    return d
+
+
+layer_epsilons = proc_all()
+make_table(layer_epsilons)
