63c136bd9ad2ce640cd16ad2a1200f05bf308da6
Emil VATAI <emil.vatai@gmail.com>
2019-03-28 13:35:05 +0900

    Added if __name__ == '__main__' to test_gpu.py.

--- src/nnclib/test_gpu.py
+++ src/nnclib/test_gpu.py
@@ -1,6 +1,7 @@
-from tensorflow.python.client import device_lib
-print(device_lib.list_local_devices())
+if __name__ == '__main__':
+    from tensorflow.python.client import device_lib
+    print(device_lib.list_local_devices())
 
-from keras import backend
-print(backend.tensorflow_backend._get_available_gpus())
+    from keras import backend
+    print(backend.tensorflow_backend._get_available_gpus())
 
