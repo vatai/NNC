3afaa4930bc1a6fb3ef03e7fd0b43fc3f59e7090
Emil VATAI <emil.vatai@gmail.com>
2019-03-28 13:38:56 +0900

    Added documentation to test_gpu.py.

--- src/nnclib/test_gpu.py
+++ src/nnclib/test_gpu.py
@@ -1,3 +1,7 @@
+"""
+This program was used to verify that the gpu is accessible.
+"""
+
 if __name__ == '__main__':
     from tensorflow.python.client import device_lib
     print(device_lib.list_local_devices())
