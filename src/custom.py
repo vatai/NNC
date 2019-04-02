51cd7f65a608635cae30558a9e92bcb8a32739c7
Emil VATAI <emil.vatai@gmail.com>
2019-04-02 22:24:33 +0900

    Compiling custom dense layer.

--- src/custom.py
+++ src/custom.py
@@ -1,36 +1,13 @@
 from nnclib.generators import CropGenerator
 from pprint import pprint
 import os
+import numpy as np
 import sacred
 import keras
+import keras.initializers
+import keras.backend as K
 
 EX = sacred.Experiment()
-
-
-class MyLayer(keras.layers.Layer):
-
-    def __init__(self, output_dim, **kwargs):
-        self.output_dim = output_dim
-        super(MyLayer, self).__init__(**kwargs)
-
-
-    def build(self, input_shape):
-        self.kernel = self.add_weight(name='kernel',
-                                      shape=(input_shape[1], self.output_dim),
-                                      initializer='uniform',
-                                      trainable=True)
-        self.bias = self.add_weight(name='bias',
-                                    shape=(self.output_dim),
-                                    initializer='uniform',
-                                    trainable=True)
-        super(MyLayer, self).build(input_shape)
-
-    def call(self, x):
-        return K.add(K.dot(x, self.kernel), self.bias)
-
-    def compute_output_shape(self, input_shape):
-        return (input_shape[0], self.output_dim)
-
 
 @EX.config
 def config():
@@ -41,7 +18,7 @@ def config():
     gen_args = {'img_dir': os.path.expanduser("~/tmp/ilsvrc/db"),
                 'val_file': os.path.expanduser("~/tmp/ilsvrc/caffe_ilsvrc12/val.txt"),
                 'batch_size': 32,
-                'fast_mode': 8}
+                'fast_mode': 1}
     eval_args = {'max_queue_size': 10,
                  'workers': 4,
                  'use_multiprocessing': True,
@@ -50,21 +27,65 @@ def config():
     seed = 42
 
 
+def my_init(shape, dtype=None):
+    # print('>>> ', shape)
+    # print('>>> ', np.zeros(shape=shape))
+    return K.variable(np.zeros(shape=shape), dtype='int32')
+
+
+class MyLayer(keras.layers.Layer):
+
+    def __init__(self, output_dim, **kwargs):
+        self.output_dim = output_dim
+        super(MyLayer, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.kernel = self.add_weight(name='kernel',
+                                      shape=(input_shape[1], self.output_dim),
+                                      initializer='uniform',
+                                      trainable=True)
+        # initializer = keras.initializers.Zeros(dtype='int32')
+        initializer = my_init
+        self.indices = self.add_weight(name='indices',
+                                       dtype='int32',
+                                       shape=(input_shape[1], self.output_dim),
+                                       initializer=initializer,
+                                       trainable=False)
+        initializer = keras.initializers.get('uniform')
+        self.bias = self.add_weight(name='bias',
+                                    shape=(self.output_dim, ),
+                                    initializer=initializer,
+                                    trainable=True)
+        super(MyLayer, self).build(input_shape)
+
+    def call(self, inputs):
+        output = K.dot(inputs, self.kernel)
+        return K.bias_add(output, self.bias, data_format='channels_last')
+
+    def compute_output_shape(self, input_shape):
+        batch_size = input_shape[0]
+        return (batch_size, self.output_dim)
+
+
 @EX.automain
 def main(compile_args, gen_args, eval_args):
     # model = keras.applications.mobilenet.MobileNet()
     model = keras.applications.vgg16.VGG16()
 
     pprint(model.layers[-4:])
-    firsthalf = keras.Model(inputs=model.input, outputs=model.layers[-4].output)
-    print(type(model.input))
-    pprint(firsthalf.layers[-1:])
 
-    
+    last = model.layers[-1]
+    fc = model.layers[-2]   # the last layer we keep
+    output_dim = K.int_shape(model.layers[-1].output)[1]
+    my_layer = MyLayer(output_dim)
+    x = my_layer(fc.output)
+
+    new_model = keras.Model(inputs=model.input, outputs=x)
 
     model.compile(**compile_args)
+    new_model.compile(**compile_args)
     generator = CropGenerator(**gen_args)
-    result = model.evaluate_generator(generator, **eval_args)
+    result = new_model.evaluate_generator(generator, **eval_args)
 
     print("Evaluation results:")
     print(result)
