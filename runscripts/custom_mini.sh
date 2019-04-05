37122ef658adde4483c6e2b6fa167bb9f5a68ff7
Emil VATAI <emil.vatai@gmail.com>
2019-04-05 14:38:02 +0900

    Cleaned up custom and custom_mini run (shell) scripts.

--- runscripts/custom_mini.sh
+++ runscripts/custom_mini.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-# Build the custom op and run custom.py
+# Build the custom op and run custom_mini.py.
+# Optionally run with $optirun.
+# Take care of LD_LIBRARY_PATH bug.
+# Take care of multiple GPUs with CUDA_VISIBLE_DEVICES
 
-OPTIRUN=optirun
-make -B -C src/unsortOp/ && LD_LIBRARY_PATH=/opt/cuda/targets/x86_64-linux/lib/ CUDA_VISIBLE_DEVICES=0 $OPTIRUN python src/custom_mini.py
+if  [ -x "$(command -v optirun)" ]; then OPTIRUN=optirun; else OPTIRUN=; fi
+OPTIRUN=     # comment this line to DISABLE OPTIRUN
+MAKEFLAG=-B  # comment this line not to force RECOMPILATION
+GPUs=0       # gpu LIST: 0 or 0,1
+make $MAKEFLAG -C src/unsortOp/ && LD_LIBRARY_PATH=/opt/cuda/targets/x86_64-linux/lib/ CUDA_VISIBLE_DEVICES=$GPUs $OPTIRUN python src/custom_mini.py
