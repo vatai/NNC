efae5e94baba2260a8b30a1a7bb2a08297a4fe98
Emil VATAI <emil.vatai@gmail.com>
2019-04-06 18:56:41 +0900

    Renamed custom_mini to custom_op_test.

--- runscripts/custom_op_test.sh
+++ runscripts/custom_op_test.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-# Build the custom op and run custom_mini.py.
+# Build the custom op and run custom_op_test.py.
 # Optionally run with $optirun.
 # Take care of LD_LIBRARY_PATH bug.
 # Take care of multiple GPUs with CUDA_VISIBLE_DEVICES
 
 if  [ -x "$(command -v optirun)" ]; then OPTIRUN=optirun; else OPTIRUN=; fi
-OPTIRUN=     # comment this line to DISABLE OPTIRUN
+# OPTIRUN=     # comment this line to DISABLE OPTIRUN
 MAKEFLAG=-B  # comment this line not to force RECOMPILATION
 GPUs=0       # gpu LIST: 0 or 0,1
-make $MAKEFLAG -C src/unsortOp/ && LD_LIBRARY_PATH=/opt/cuda/targets/x86_64-linux/lib/ CUDA_VISIBLE_DEVICES=$GPUs $OPTIRUN python src/custom_mini.py
+make $MAKEFLAG -C src/unsortOp/ && LD_LIBRARY_PATH=/opt/cuda/targets/x86_64-linux/lib/ CUDA_VISIBLE_DEVICES=$GPUs $OPTIRUN python src/custom_op_test.py
