07ea6a16b90c6f9b759d51911a8f02217be47b96
Emil VATAI <emil.vatai@gmail.com>
2019-04-06 19:06:58 +0900

    Added custom_op_test docstring.

--- src/custom_op_test.py
+++ src/custom_op_test.py
@@ -1,17 +1,18 @@
+"""Simple script used to test the correctness of the custom op
+``unsort``.
+
+The expected behaviour is that the last two matrices printed
+(``exact_output`` and ``output``) to be similar (or the same).
+
 """
-.. todo::
-
-    Rename the other one to ``custom_op_test.py``.
-"""
-def eval_print(x):
-    print(K.eval(x))
-
-
-def shape_print(x):
-    print(K.int_shape(x))
 
 
 def decompose(weights):
+    """This function converts the weights (the ``kernel`` matrix) to the
+    approximating representation of ``indices`` and ``mean``.  See
+    details in :py:class:`custom_layer_test.CompressedPrototype`.
+
+    """
     indices = np.argsort(weights, axis=0)
     sorted_weights = np.take_along_axis(weights, indices, axis=0)
     mean = np.mean(sorted_weights, axis=1)
@@ -19,16 +20,23 @@ def decompose(weights):
 
 
 if __name__ == '__main__':
+    import os
+
     import numpy as np
     import tensorflow
     import keras.backend as K
+
+    # Suppress warnings.
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
+
+    # Load module from ``.so``.
     unsort_module = tensorflow.load_op_library('./src/unsortOp/unsort_ops.so')
 
-    # inputs = np.random.randn(4, 3)
+    # Basically a random input batch.  Here 
     inputs = np.array([[ 0.71238331,  1.38283577,  0.37231196],
-                    [ 0.08651744, -1.05230163,  1.00697538],
-                    [-1.56814483, -0.30826115, -0.13245332],
-                    [-0.55250484, -1.53117801,  1.65068967]])
+                       [ 0.08651744, -1.05230163,  1.00697538],
+                       [-1.56814483, -0.30826115, -0.13245332],
+                       [-0.55250484, -1.53117801,  1.65068967]])
     inputs_tensor = K.variable(inputs)
 
     weights = np.array([[-0.5, 0.5],
@@ -39,8 +47,8 @@ if __name__ == '__main__':
     mean_tensor = K.variable(mean)
 
     output = unsort_module.unsort(inputs_tensor,
-                                indices_tensor,
-                                mean_tensor)
+                                  indices_tensor,
+                                  mean_tensor)
 
     print("inputs")
     print(inputs)
@@ -59,5 +67,4 @@ if __name__ == '__main__':
     print(exact_output)
 
     print('output')
-    eval_print(output)
-
+    print(K.eval(output))
