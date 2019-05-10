819d41d8869a7dfb605add871cc8afdc141b53e1
Emil VATAI <emil.vatai@gmail.com>
2019-05-10 15:59:51 +0900

    Add multi gpu support

--- src/nnclib/experiments/collection.py
+++ src/nnclib/experiments/collection.py
@@ -2,6 +2,7 @@ from os.path import expanduser
 
 from keras.applications.inception_resnet_v2 import InceptionResNetV2
 from keras.layers import Dense, Conv2D
+from keras.utils import multi_gpu_model
 from sacred import Experiment
 
 from nnclib.compression import weights_updater, reshape_norm_meld
@@ -14,6 +15,7 @@ inceptionresnetv2_experiment = Experiment()
 def _inceptionresnetv2_config():
     # pylint: disable=unused-variable
     # flake8: noqa: F481
+    gpus = 1
     compile_args = {
         'optimizer': 'rmsprop',
         'loss': 'categorical_crossentropy',
@@ -32,8 +34,10 @@ def _inceptionresnetv2_config():
 
 
 @inceptionresnetv2_experiment.main
-def _inceptionresnetv2_main(compile_args, gen_args, updater_list):
+def _inceptionresnetv2_main(gpus, compile_args, gen_args, updater_list):
     model = InceptionResNetV2()
+    if gpus > 1:
+        model = multi_gpu_model(model)
     weights_updater(model, updater_list)
     model.compile(**compile_args)
     results = model.evaluate_generator(CropGenerator(**gen_args))
