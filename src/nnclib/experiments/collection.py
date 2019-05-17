3a3fd9f0c3b77f5caeffaac1eb6a76a82c0f5be2
Emil VATAI <emil.vatai@gmail.com>
2019-05-17 22:19:33 +0300

    Fix mnist processing

--- src/nnclib/experiments/collection.py
+++ src/nnclib/experiments/collection.py
@@ -2,18 +2,19 @@
 
 TODO(vatai): extract delta: if has '-' use partial
 
-TODO(vatai): fix mnist https://www.kaggle.com/anandad/classify-fashion-mnist-with-vgg16
+TODO(vatai): CHECK fix mnist https://www.kaggle.com/anandad/classify-fashion-mnist-with-vgg16
 """
 
 # from functools import partial
 import numpy as np
 
-import keras.datasets
 from keras.layers import Dense, Conv2D
 from keras.models import Model
+from keras.preprocessing.image import img_to_array, array_to_img
 from keras.utils import multi_gpu_model
 from sacred import Experiment
 from sacred.observers import FileStorageObserver
+import keras.datasets
 
 import nnclib.compression
 from nnclib.model_dict import model_dict
@@ -65,11 +66,11 @@ def _legion_config():
     compile_args = {
         'optimizer': 'rmsprop',
         'loss': 'sparse_categorical_crossentropy',
-        'metrics': ['categorical_accuracy',
-                    'top_k_categorical_accuracy'],
+        'metrics': ['sparse_categorical_accuracy',
+                    'sparse_top_k_categorical_accuracy'],
     }
     fit_args = {
-        'epochs': 2,
+        'epochs': 1,
         # 'validation_split': 0.2,
         'verbose': 2,
         'batch_size': 128,
@@ -84,16 +85,32 @@ def _legion_main(experiment_args, compile_args, fit_args):
     dataset = dataset_dict[dataset_name]
     train_data, test_data = dataset.load_data()
     output_units = np.max(train_data[1]) + 1
-    input_shape = train_data[0].shape[1:]
-    # if len(input_shape) < 3:
-    #     input_shape = tuple(list(input_shape) + [1])
-    # print('>>>> input_shape', input_shape)
+
+    shape = train_data[0].shape[1:]
+    if len(shape) < 3:
+        shape = [-1] + list(shape) + [3]
+
+        train_x, train_t = train_data
+        test_x, test_t = test_data
+
+        train_x = np.dstack([train_x] * 3)
+        test_x = np.dstack([test_x] * 3)
+
+        train_x = np.reshape(train_x, shape)
+        test_x = np.reshape(test_x, shape)
+
+        train_x = np.asarray([img_to_array(array_to_img(im, scale=False).resize((48, 48))) for im in train_x])
+        test_x = np.asarray([img_to_array(array_to_img(im, scale=False).resize((48, 48))) for im in test_x])
+
+        train_data = train_x, train_t
+        test_data = test_x, test_t
+
+    print('>>>>', train_data[0].shape)
 
     # Model
     model_name = experiment_args['model_name']
     model_class, preproc_dict = model_dict[model_name]
-    base_model = model_class(input_shape=input_shape,
-                             include_top=False, pooling='avg')
+    base_model = model_class(weights=None, include_top=False, pooling='avg')
     outputs = base_model.output
     outputs = Dense(output_units, activation='softmax')(outputs)
     model = Model(inputs=base_model.input, outputs=outputs)
@@ -111,6 +128,6 @@ def _legion_main(experiment_args, compile_args, fit_args):
     model.fit(*train_data, **fit_args)
 
     #eval
-    result = model.evaluate_generator(*test_data)
+    result = model.evaluate(*test_data)
     results = dict(zip(['loss', 'top1', 'top5'], result))
     return results
