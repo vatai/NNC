128f47942cc96f4746c6bac101234a8e84e5accd
Emil VATAI <emil.vatai@gmail.com>
2019-05-20 16:57:06 +0900

    [Fix] decode_updater_list

--- src/nnclib/experiments/collection.py
+++ src/nnclib/experiments/collection.py
@@ -85,6 +85,8 @@ def decode_updater_list(coded_updater_list):
         'nm': nnclib.compression.reshape_norm_meldprune
     }
 
+    if ',' not in coded_updater_list:
+        return []
     updater_list = []
     for pair in coded_updater_list.split(';'):
         typ, upd = pair.split(',')
@@ -139,6 +141,7 @@ def _experiment_name(experiment_args):
     tokens.append(experiment_args['coded_updater_list'])
     tokens.append(str(experiment_args['on_nth_epoch']))
     return '-'.join(tokens)
+
 
 @legion_experiment.main
 def _legion_main(_seed, experiment_args, compile_args, fit_args):
