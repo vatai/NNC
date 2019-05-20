6aa42ae71ea3744f06ab8e5786f3df2d16cf8abb
Emil VATAI <emil.vatai@gmail.com>
2019-05-20 18:46:57 +0900

    Add 'val_' to histv.py

--- src/histv.py
+++ src/histv.py
@@ -7,6 +7,6 @@ import sys
 file_list = glob.glob(sys.argv[1])
 for file_name in file_list:
     hist = pickle.load(open(file_name, "rb"))
-    result = hist.history['sparse_categorical_accuracy']
+    result = hist.history['val_sparse_categorical_accuracy']
     print(file_name)
     print(result)
